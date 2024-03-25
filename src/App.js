dd4f15d5d0eb62212785c98de4d45523c14a615e
nurticcip <nurticcip7@gmail.com>
2024-03-25 15:12:44 +0600

    r

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
-import './App.scss';
-import Footer from './components/Footer';
-import Menu from './components/Menu';
-=======
 import "./App.scss";
 import MyRoutes from "./MyRoutes";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Home from "./pages/Home";
->>>>>>> main
 
 function App() {
   return (
     <div className="app">
-<<<<<<< HEAD
-      <Menu />
-      <Footer/>
-=======
       <Header />
       <MyRoutes />
->>>>>>> main
+      <Footer />
     </div>
   );
 }
